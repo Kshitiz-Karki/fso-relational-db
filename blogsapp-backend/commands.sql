5bdf5ee32c5aff7743eb843b8efdca101b761b29
Kshitiz Karki <karki.kshitiz.23@gmail.com>
2023-12-04 21:33:42 +0545

    add solutions till exercise 13.7

---
+++ blogsapp-backend/commands.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes integer DEFAULT 0
+);
+
+
+insert into blogs (author, url, title) values ('Nathan Sebhastian', 'https://www.freecodecamp.org/news/javascript-arrow-functions-in-depth/', 'How to Use JavaScript Arrow Functions - Explained in Detail');
+insert into blogs (author, url, title) values ('David Deal', 'https://hackernoon.com/rick-rubin-and-the-human-touch-can-ai-replace-human-instinct', 'Can AI Replace Human Instinct?');
---
+++ blogsapp-backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
+const blogsRouter = require('./controllers/blogs')
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  return response.status(400).send({ error })
+  // next(error)
+}
+app.use(errorHandler)
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+start()
